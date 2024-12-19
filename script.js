dd459e392b11b995f238a563c7f8a00f457a1ed5
Magma71767 <a47169@alunos.ipb.pt>
2024-12-19 18:35:05 +0000

    ATUALIZAÇÃO

--- script.js
+++ script.js
@@ -27,3 +27,19 @@ document.addEventListener("DOMContentLoaded", () => {
   
     rolarCarrossel();
 });
+
+let currentSlide = 0;
+
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+
+function changeSlide(index) {
+
+    slides.forEach((slide) => slide.classList.remove('active'));
+    dots.forEach((dot) => dot.classList.remove('active'));
+
+    slides[index].classList.add('active');
+    dots[index].classList.add('active');
+
+    currentSlide = index;
+}
