969267debcbee18f7555f08c07fb3a37e047a766
doncanjas <80267631+DonCanjas@users.noreply.github.com>
2025-01-17 16:06:54 +0000

    ad eng a contactos

--- idioma.js
+++
@@ -1,9 +0,0 @@
-function changeLanguage(language) {
-    localStorage.setItem('language', language);  // Store language choice
-
-    if (language === 'eng') {
-        window.location.href = "index_eng.html"; // Redirect to English version
-    } else {
-        window.location.href = "index.html"; // Redirect to Portuguese version
-    }
-}
